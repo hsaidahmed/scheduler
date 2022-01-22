4c6669c46a213e8449afbad785b82048de006137
hsaidahmed <hashim.saidahmed@gmail.com>
2022-01-21 19:05:30 -0500

    building interviewer list item and interviewer list

---
+++ src/components/DayListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+
+export default function DayListItem(props) {
+  const formatSpots = (spots) => {
+    if (!spots) return 'no spots remaining';
+    if (spots === 1 ) return '1 spot remaining';
+    return `${spots} spots remaining`;
+
+  }
+  const dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0});
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{formatSpots(props.spots)} </h3>
+    </li>
+  );
+}
